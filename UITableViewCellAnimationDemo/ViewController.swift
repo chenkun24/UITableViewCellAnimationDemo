82ba3bbc0eac4cc27c485c1ac999185cdd668d2f
chenkun24 <chenkun0824@gmail.com>
2015-09-06 22:06:08 +0800

    views and animation

--- UITableViewCellAnimationDemo/ViewController.swift
+++ UITableViewCellAnimationDemo/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    var shouldShowNotification = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let bottomView = UIView(frame: CGRectZero)
+        self.tableView.tableFooterView = bottomView
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func notifyAction(sender: UISwitch) {
+        
+        self.shouldShowNotification = sender.on
+        self.datePicker.hidden = !sender.on
+        
+        // method 1
+//        self.tableView.reloadData()
+        
+        // method 2
+        self.tableView.beginUpdates()
+        self.tableView.endUpdates()
+    }
 
+}
+
+extension ViewController {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        if indexPath.row == 1 && self.shouldShowNotification
+        {
+            return 211
+        }
+        
+        return 44
+    }
+    
+    
 }
 
